93b2480c662862a6dcf0962333c17465dc6cf108
Christine <christine.sulio@gmail.com>
2025-09-25 17:26:34 -0700

    Added initial HTML structure, navbar, and styles

---
+++ script.js
@@ -0,0 +1,7 @@
+// Fetch navbar.html and inject it into #navbar div
+fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(error => console.error("Error loading navbar:", error));
